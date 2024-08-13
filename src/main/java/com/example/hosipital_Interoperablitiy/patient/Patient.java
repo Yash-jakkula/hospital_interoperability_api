aef9ce4d0cfa5c329e40d878d761d5033e208a46
yashwanthJakkula <yashjksoft@gmail.com>
2024-08-14 02:07:11 +0530

    added custom query starter

--- src/main/java/com/example/hosipital_Interoperablitiy/patient/Patient.java
+++ src/main/java/com/example/hosipital_Interoperablitiy/patient/Patient.java
@@ -1,6 +1,13 @@
 package com.example.hosipital_Interoperablitiy.patient;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface Patient extends CrudRepository<PatientSchema,Integer> {
+    @Query(value = "select * from patient_schema where id = ?1", nativeQuery = true)
+    Optional<PatientSchema> patient_meta_data(String id);
+
+
 }
--- src/main/java/com/example/hosipital_Interoperablitiy/patient/PatientController.java
+++ src/main/java/com/example/hosipital_Interoperablitiy/patient/PatientController.java
@@ -2,6 +2,7 @@ package com.example.hosipital_Interoperablitiy.patient;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,8 @@ public class PatientController {
     private static final Logger log = LoggerFactory.getLogger(PatientController.class);
     private final Patient patient;
 
-    private PatientController(Patient patient) {
+    @Autowired
+    public PatientController(Patient patient) {
         this.patient = patient;
     }
 
@@ -58,47 +60,16 @@ public class PatientController {
         return ResponseEntity.ok(true);
     }
 
-//    public String longestPalindrome() {
-//        String s = "abababscceadfkaskdfjakkkkkkkreeefadfase";
-//        StringBuilder sb = new StringBuilder(s);
-//        if(s.equals(sb.reverse().toString())) return s;
-//        if(s.length() == 1) return s;
-//        int len = s.length()-1;
-//        String s2="";
-//        for(int k=len-1;k>0;k--){
-//            for(int i=0;i+k<=len;i++){
-//                s2 = s.substring(i,i+k+1);
-//                StringBuilder check = new StringBuilder(s2);
-//                if(s2.equals(check.reverse().toString())){
-//                    if(!s2.equals(""))
-//                        return s2;
-//                }
-//            }
-//        }
-//        return Character.toString(s.charAt(0));
-//    }
-//    public String longestPalindrome() {
-//        String s = "abababscceadfkaskdfjakkkkkkkreeefadfase";
-//
-//        StringBuilder sb = new StringBuilder(s);
-//        if(s.equals(sb.reverse().toString())) return s;
-//        if(s.length() == 1) return s;
-//        int k = s.length()-1;
-//        String s2="";
-//        for(int i=k;i>=0;i--){
-//            for(int j=0;j<s.length();j++){
-//                for(int l=j+i;l<=s.length();l++){
-//                    s2 = s.substring(j,l);
-//                    StringBuilder check = new StringBuilder(s2);
-//                    if(s2.equals(check.reverse().toString())){
-//                        if(!s2.equals(""))
-//                            return s2;
-//                    }
-//                }
-//            }
-//        }
-//        return Character.toString(s.charAt(0));
-//    }
+    @GetMapping("/getmetadata/{id}")
+    public ResponseEntity<Optional<PatientSchema>> getMetaData(@PathVariable String id){
+        Optional<PatientSchema> meta_data = patient.patient_meta_data(id);
+        if(meta_data.isPresent()){
+            return ResponseEntity.ok(meta_data);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Optional.empty());
+        }
+    }
 
 }
 
